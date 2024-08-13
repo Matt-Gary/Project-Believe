cd6586102772bdcf3ba4c258ae11553e4e8e8546
Matt-Gary <matgarbrasil@gmail.com>
2024-08-13 16:56:30 -0300

    Creating Users Database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const cors = require("cors")
+const db = require('./models') //connect database
 
 const app = express()
-
-app.use(express.json())
+app.use(express.json()) ////allow to access the body being parse (send in json)
 app.use(cors())
 
-app.listen(4000, () => {
-    console.log("Server on localhost: 4000")
+db.sequelize.sync().then(() => {
+    app.listen(4000, () => console.log("Server on localhost:4000"))
 })
