7ae8cd062a038eb0814a081f76310ebc6400ac5f
Matt-Gary <matgarbrasil@gmail.com>
2024-08-15 14:08:20 -0300

    Creating registration (auth) endpoint/ testing connection

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,4 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const {Users} = require('../models')
+const bcrypt = require("bcrypt") //hash our password in the table
 
+//insert data into our table
+
+router.post("/", async (req,res) => {
+    const {username, password, email, matricula} = req.body;
+    bcrypt.hash(password, 10).then((hash) => {
+        Users.create({
+            username: username,
+            password: hash,
+            matricula: matricula,
+            email: email,
+        })
+        .then(() => res.json("success"))
+    }); 
+})
+
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
-const express = require("express")
-const cors = require("cors")
-const db = require('./models') //connect database
+const express = require("express");
+const cors = require("cors");
+const db = require('./models');
 
-const app = express()
-app.use(express.json()) ////allow to access the body being parse (send in json)
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const usersRouter = require('./routes/Users');
+app.use('/auth', usersRouter);
 
 db.sequelize.sync().then(() => {
-    app.listen(4000, () => console.log("Server on localhost:4000"))
-})
+    app.listen(4000, () => {
+        console.log("Server on localhost:4000");
+    });
+});
