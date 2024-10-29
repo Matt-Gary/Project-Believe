780c482b7dd28201e2425356ae1d804d382e80d0
Matt-Gary <matgarbrasil@gmail.com>
2024-10-29 17:57:40 -0300

    folder structure change

--- server/api/server.js
+++ server/api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const db = require('./models');
+const db = require('../models');
 const cookieParser = require("cookie-parser")
 const path = require('path')
 require('dotenv').config()
@@ -10,20 +10,20 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser())
 //Creating path for our registration
-const usersRouter = require('./routes/Users');
+const usersRouter = require('../routes/Users');
 app.use('/auth', usersRouter);
 
-const eventRoutes = require('./routes/Gallery');
+const eventRoutes = require('../routes/Gallery');
 app.use('/gallery', eventRoutes);
 
-const benefitsRoutes = require('./routes/Benefits');
+const benefitsRoutes = require('../routes/Benefits');
 app.use('/benefits', benefitsRoutes);
 
-const tutorialsRouter = require('./routes/Tutorials');
+const tutorialsRouter = require('../routes/Tutorials');
 app.use('/tutorial', tutorialsRouter);
 
 // Uploading photos
-app.use('/uploades/ProfileImages', express.static(path.join(__dirname, 'ProfileImages')))
+app.use('../uploades/ProfileImages', express.static(path.join(__dirname, 'ProfileImages')))
 
 db.sequelize.sync().then(() => {
     const PORT = process.env.PORT || 4000;
