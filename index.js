94a46ba10bed149dcb76ebc0d2b61ed72c611dcc
Matt-Gary <matgarbrasil@gmail.com>
2024-10-29 20:20:10 -0300

    modify vercel, create separate index,js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const db = require('./models');
+const cookieParser = require("cookie-parser")
+const path = require('path')
+require('dotenv').config()
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser())
+//Creating path for our registration
+const usersRouter = require('./routes/Users');
+app.use('/auth', usersRouter);
+
+const eventRoutes = require('./routes/Gallery');
+app.use('/gallery', eventRoutes);
+
+const benefitsRoutes = require('./routes/Benefits');
+app.use('/benefits', benefitsRoutes);
+
+const tutorialsRouter = require('./routes/Tutorials');
+app.use('/tutorial', tutorialsRouter);
+
+// Uploading photos
+app.use('../uploades/ProfileImages', express.static(path.join(__dirname, 'ProfileImages')))
+
+db.sequelize.sync().then(() => {
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
+
+module.exports = app
