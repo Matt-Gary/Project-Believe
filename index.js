4bc69452b5e03d894c3ca5081d7efb83a41a5d49
Matt-Gary <matgarbrasil@gmail.com>
2024-11-07 16:05:31 -0300

    Update

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.header(
-    "Access-Control-Allow-Origin",
-    "https://localhost:5173"
-    );
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Ensure this matches your frontend's URL
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Credentials", "true");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200); // Respond OK to preflight requests
+    }
     
     console.log("Request received:", req.method, req.url);
-    
     next();
-    });
+});
 
 app.use(cookieParser())
 //Creating path for our registration
