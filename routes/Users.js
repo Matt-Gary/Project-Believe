0c726e6d9f2ef0366ea1d9045a90c7114b1ce743
Matt-Gary <matgarbrasil@gmail.com>
2024-12-01 19:02:39 -0300

    Change of feedback at login

--- routes/Users.js
+++ routes/Users.js
@@ -45,7 +45,7 @@ router.post("/register", async (req, res) => {
 
     // Validate email format before proceeding
     if (!emailRegex.test(email)) {
-        return res.status(400).json({error: "Invalid email format"})
+        return res.status(400).json({error: "Formato de e-mail inválido"})
     }
 
     try {
@@ -55,11 +55,11 @@ router.post("/register", async (req, res) => {
         const existingMatricula = await Users.findOne({ where: { matricula: matricula } });
         // Validation: Check if matricula is already in use
         if (existingMatricula) {
-            return res.status(400).json({error: "Matricula is already used"})
+            return res.status(400).json({error: "Matrícula já é usada"})
         }
         // Validation: Check if the email is already registered
         if (existingUser) {
-            return res.status(400).json({ error: "User already exists" });
+            return res.status(400).json({ error: "O usuário já existe" });
         }
 
         // Hash the password before storing it in the database
@@ -96,13 +96,13 @@ router.post("/login", async (req, res) => {
         const user = await Users.findOne({where: {email}})
         // Validation: Check if the user exists
         if(!user) {
-            return res.status(400).json({message: "Invalid Credenstials"})    
+            return res.status(400).json({message: "Credenciais inválidas"})    
         }
         // Compare the provided password with the stored hashed password
         const isPasswordMatch = await bcrypt.compare(password, user.password)
         // Validation: Check if the password matches
         if(!isPasswordMatch) {
-            return res.status(400).json({message: "Invalid Credenstials"})    
+            return res.status(400).json({message: "Credenciais inválidas"})    
         }
         // Create a JSON Web Token (JWT) for the user
         const token = jwt.sign({
